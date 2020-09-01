571d46785385286da8f550a10853f345bc4234db
dconceicao <dconceicao@rpc.com.br>
2020-09-01 01:12:48 -0300

    Ajuste DIV

--- app-web/src/views/dashViews/viagens/components/FiltersComponent.jsx
+++ app-web/src/views/dashViews/viagens/components/FiltersComponent.jsx
@@ -2,30 +2,28 @@ import React from 'react'
 import { FiltersToolBar, InputText } from '../../../../components'
 
 export default ({ setOpenAddViagens }) => (
-  <div>
-    <FiltersToolBar
-      rightButton
-      addClick={() => setOpenAddViagens(true)}
-      searchClick={() => console.log('searchClick OK')}
-    >
-      <InputText
-        id={'standard-basic-1'}
-        label={'Teste'}
-        placeholder={'Teste'}
-        onChange={() => console.log('Teste')}
-      />
-      <InputText
-        id={'standard-basic-2'}
-        label={'Teste'}
-        placeholder={'Teste'}
-        onChange={() => console.log('Teste')}
-      />
-      <InputText
-        id={'standard-basic-3'}
-        label={'Teste'}
-        placeholder={'Teste'}
-        onChange={() => console.log('Teste')}
-      />
-    </FiltersToolBar>
-  </div>
+  <FiltersToolBar
+    rightButton
+    addClick={() => setOpenAddViagens(true)}
+    searchClick={() => console.log('searchClick OK')}
+  >
+    <InputText
+      id={'standard-basic-1'}
+      label={'Teste'}
+      placeholder={'Teste'}
+      onChange={() => console.log('Teste')}
+    />
+    <InputText
+      id={'standard-basic-2'}
+      label={'Teste'}
+      placeholder={'Teste'}
+      onChange={() => console.log('Teste')}
+    />
+    <InputText
+      id={'standard-basic-3'}
+      label={'Teste'}
+      placeholder={'Teste'}
+      onChange={() => console.log('Teste')}
+    />
+  </FiltersToolBar>
 )
